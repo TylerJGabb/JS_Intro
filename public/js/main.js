b688f9a06c9de6380d8ab436a9e1671b7c06ea9e
tylerjgabb <tylerjgabb@gmail.com>
2018-01-13 19:36:55 -0500

    wtf

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,27 @@
-$(function(){//when the page is finished loading
-    $("#background-change").on('click', function(){//attach this listener to the backroung-change button
-        $("body").toggleClass('brown-background');
-    });
-});
+var tester = document.createElement("div");
+tester.style = "width:600px;height:250px;border-color:black"
+tester.id = "tester";
+document.body.appendChild(tester);
+
+function squareArray(arr){
+    var ret = [];
+    for(var x in arr){
+        ret.push(x*x);
+    }
+    return ret;
+}
+
+var data = [1,2,3,4,5,6,7,8,9,10];
+var par = document.createElement("p");
+par.innerHTML = data.toString() + "<br>" + squareArray(data).toString();
+document.body.appendChild(par);
+
+Plotly.plot(tester,
+    [
+        {
+            x : data,
+            y : squareArray(data)
+        }
+    ],
+    {margin: {t : 1 } }
+);
